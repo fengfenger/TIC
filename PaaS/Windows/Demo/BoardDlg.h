d53e2cc5252e08cb0e523eddf55426c52c6dcd1c
jepsonyang <jepsonyang@tencent.com>
2019-12-02 17:50:56 +0800

    TIC Demo支持文档转码

--- PaaS/Windows/Demo/BoardDlg.h
+++ PaaS/Windows/Demo/BoardDlg.h
@@ -171,20 +171,30 @@ private:
 	afx_msg void OnCbnSelchangeComboFitMode();
 
 	//TEduBoardCallback
+	//通用事件回调
 	virtual void onTEBError(TEduBoardErrorCode code, const char * msg) override;
 	virtual void onTEBWarning(TEduBoardWarningCode code, const char * msg) override;
+	//基本流程回调
 	virtual void onTEBInit() override;
 	virtual void onTEBHistroyDataSyncCompleted() override;
 	virtual void onTEBSyncData(const char * data) override;
 	virtual void onTEBUndoStatusChanged(bool canUndo) override;
 	virtual void onTEBRedoStatusChanged(bool canRedo) override;
+	virtual void onTEBOffscreenPaint(const void* buffer, uint32_t width, uint32_t height) {};
+	//涂鸦功能回调
 	virtual void onTEBImageStatusChanged(const char * boardId, const char * url, TEduBoardImageStatus status) override {}
 	virtual void onTEBSetBackgroundImage(const char * url) override {}
+	virtual void onTEBBackgroundH5StatusChanged(const char *boardId, const char *url, TEduBoardBackgroundH5Status status) {};
+	//白板页操作回调
 	virtual void onTEBAddBoard(const TEduBoardStringList *boardList, const char * fileId) override;
 	virtual void onTEBDeleteBoard(const TEduBoardStringList *boardList, const char * fileId) override;
 	virtual void onTEBGotoBoard(const char * boardId, const char * fileId) override;
-	virtual void onTEBAddFile(const char * fileId) override;
-	virtual void onTEBAddH5PPTFile(const char * fileId) override;
+	virtual void onTEBGotoStep(uint32_t currentStep, uint32_t totalStep) {};
+	//文件操作回调
+	virtual void onTEBFileTranscodeProgress(const char *path, const char *errorCode, const char *errorMsg, const TEduBoardTranscodeFileResult &result);
+	virtual void onTEBAddTranscodeFile(const char *fileId);
+	virtual void onTEBVideoStatusChanged(const char *fileId, TEduBoardVideoStatus status, double progress, double duration) {};
+	virtual void onTEBH5FileStatusChanged(const char *fileId, TEduBoardH5FileStatus status) {};
 	virtual void onTEBDeleteFile(const char * fileId) override;
 	virtual void onTEBSwitchFile(const char * fileId) override;
 	virtual void onTEBFileUploadProgress(const char *fileId, int currentBytes, int totalBytes, int uploadSpeed, double percent) override {};
