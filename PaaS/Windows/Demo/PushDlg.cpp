56eff3ef6d55ec7aa9cdd4212de9572d32a8f87d
sevenzhu <sevenzhu@tencent.com>
2020-01-07 17:14:28 +0800

    [录制]增加录制的demo.

--- PaaS/Windows/Demo/PushDlg.cpp
+++ PaaS/Windows/Demo/PushDlg.cpp
@@ -9,6 +9,7 @@
 BEGIN_MESSAGE_MAP(CPushDlg, CDialogEx)
 	ON_BN_CLICKED(IDC_CHK_ENABLE_DRAW, &CPushDlg::OnBnClickedChkEnableDraw)
 	ON_BN_CLICKED(IDC_CHECK_ENABLE_PUSH, &CPushDlg::OnBnClickedCheckEnablePush)
+	ON_NOTIFY(LVN_ITEMCHANGED, IDC_LIST_LOCAL_RECORD, &CPushDlg::OnLvnItemchangedListLocalRecord)
 END_MESSAGE_MAP()
 
 CPushDlg::CPushDlg(CWnd* pParent)
@@ -21,24 +22,14 @@ CPushDlg::CPushDlg(CWnd* pParent)
 BOOL CPushDlg::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
-	int appid = 1400162216;
-	std::string user = "seven";
-	std::string sig = "eJxlz01Pg0AQgOE7v4JwNrI7y0fwhqXEjW5jaW2rF4IwrQvhQ1gprfG-G7GJJM71eSeT*dR0XTfWD6vrJE3rj0rF6tSgod-oBjGu-rBpZBYnKmZt9g9xaGSLcbJX2I5IbdsGQqaNzLBSci8vRYc9VhPusiIeb-zuW4RQB4A600QeRhTz5xlfBgNAyML5orPNyIfHA3Bx3Ibvd*n5NZDcX1vwFKV5EK7ul-zNX4jZrtwUL3lSF7fbjpcpjfIdioFw5onENXt-45rijMFxclLJEi8POR7zLOrCRHtsO1lXYwCE2hQY*RlD*9K*AYzpXAs_";
-	TEduRecordAuthParam auth(appid, user, sig);
 
-	std::weak_ptr< CPushDlg> weakSelf = this->shared_from_this();
-	mLocalRecorder->init(auth, [this, weakSelf](TICModule module, int code, const char *desc) {
-		std::shared_ptr<CPushDlg> self = weakSelf.lock();
-		if (!self)
-		{
-			return;
-		}
+	mListRecord.InsertColumn(0, _T("课堂号"), LVCFMT_LEFT, 80);
+	mListRecord.InsertColumn(1, _T("文件名"), LVCFMT_LEFT, 100);
+	mListRecord.InsertColumn(2, _T("页码"), LVCFMT_LEFT, 72);
 
-		if (code != 0) {
-			AfxMessageBox(_T("认证失败!"), MB_OK);
-		}
-	});
+	initRecord();
 
+	getRecord();
 
 	return TRUE;
 }
@@ -48,6 +39,7 @@ void CPushDlg::DoDataExchange(CDataExchange* pDX)
 	CDialogEx::DoDataExchange(pDX);
 
 	DDX_Control(pDX, IDC_CHECK_ENABLE_PUSH, chkPushEnable_);
+	DDX_Control(pDX, IDC_LIST_LOCAL_RECORD, mListRecord);
 }
 
 
@@ -58,54 +50,14 @@ void CPushDlg::OnBnClickedChkEnableDraw()
 
 void CPushDlg::OnBnClickedCheckEnablePush()
 {
-	if (mLocalRecorder)
-	{
-		bool selected = chkPushEnable_.GetCheck() == BST_CHECKED;
-		std::weak_ptr< CPushDlg> weakSelf = this->shared_from_this();
+	bool selected = (chkPushEnable_.GetCheck() == BST_CHECKED);
 
-		if (selected) {
-			TEduRecordParam para;
-			para.AppProc = "TICDemo.exe";
-			const std::string url = "rtmp://29734.livepush.myqcloud.com/live/seventest?txSecret=b356ff74acb71c6d1a758719d557fc9c&txTime=5DE291FF";
-			mLocalRecorder->startPush(para, url.c_str(), [this, weakSelf](TICModule module, int code, const char *desc) {
-				std::shared_ptr<CPushDlg> self = weakSelf.lock();
-				if (!self)
-				{
-					return;
-				}
-
-				if (code != 0) {
-					AfxMessageBox(_T("开始推流失败"), MB_OK);
-				}
-				else {
-					AfxMessageBox(_T("开始推流"), MB_OK);
-					self->StartPlay();
-				}
-			});
-		}
-		else {
-			mLocalRecorder->stopPush([this, weakSelf](TICModule module, int code, const char *desc) {
-				std::shared_ptr<CPushDlg> self = weakSelf.lock();
-				if (!self)
-				{
-					return;
-				}
-
-				self->StopPlay();
-				if (code != 0) {
-					AfxMessageBox(_T("停止推流失败"), MB_OK);
-				}
-				else {
-					AfxMessageBox(_T("停止推流"), MB_OK);
-				}
-			});
-		}
-
-
+	if (selected) {
+		startRecord();
 	}
-
-
-
+	else {
+		stopRecord();
+	}
 }
 
 void CPushDlg::StartPlay() {
@@ -119,3 +71,158 @@ void CPushDlg::StopPlay() {
 	mLivePlayer->stopPlay();
 }
 
+
+void CPushDlg::UpdateFileList()
+{
+	mListRecord.SetRedraw(FALSE);
+	mListRecord.DeleteAllItems();
+
+
+			/*
+			auto* fileList = boardCtrl->GetFileInfoList();
+
+			if (!fileList) return;
+			std::string curFileId = boardCtrl->GetCurrentFile();
+			for (uint32_t i = 0; i < fileList->GetCount(); ++i)
+			{
+				TEduBoardFileInfo fileInfo = fileList->GetFileInfo(i);
+				listFile_.InsertItem(i, _T(""));
+				listFile_.SetItemText(i, 0, a2w(fileInfo.fileId, CP_UTF8).c_str());
+				listFile_.SetItemText(i, 1, a2w(fileInfo.title, CP_UTF8).c_str());
+				listFile_.SetItemText(i, 2, (std::to_wstring(fileInfo.pageIndex + 1) + _T("/") + std::to_wstring(fileInfo.pageCount)).c_str());
+				if (curFileId == fileInfo.fileId) // 选中当前文件
+				{
+					listFile_.SetItemState(i, LVIS_SELECTED | LVIS_FOCUSED, LVIS_SELECTED | LVIS_FOCUSED);
+					listFile_.SetFocus();
+				}
+			}
+			fileList->Release();
+			listFile_.SetRedraw(TRUE);
+			*/
+
+
+
+}
+
+void CPushDlg::initRecord() {
+	int appid = 1400162216;
+	std::string user = "seven";
+	std::string sig = "eJxlz01Pg0AQgOE7v4JwNrI7y0fwhqXEjW5jaW2rF4IwrQvhQ1gprfG-G7GJJM71eSeT*dR0XTfWD6vrJE3rj0rF6tSgod-oBjGu-rBpZBYnKmZt9g9xaGSLcbJX2I5IbdsGQqaNzLBSci8vRYc9VhPusiIeb-zuW4RQB4A600QeRhTz5xlfBgNAyML5orPNyIfHA3Bx3Ibvd*n5NZDcX1vwFKV5EK7ul-zNX4jZrtwUL3lSF7fbjpcpjfIdioFw5onENXt-45rijMFxclLJEi8POR7zLOrCRHtsO1lXYwCE2hQY*RlD*9K*AYzpXAs_";
+	TEduRecordAuthParam auth(appid, user, sig);
+
+	std::weak_ptr< CPushDlg> weakSelf = this->shared_from_this();
+	mLocalRecorder->init(auth, [this, weakSelf](TICModule module, int code, const char *desc) {
+		std::shared_ptr<CPushDlg> self = weakSelf.lock();
+		if (!self)
+			return;
+
+		if (code != 0) {
+			AfxMessageBox(_T("认证失败!"), MB_OK);
+		}
+	});
+}
+
+void CPushDlg::startRecord() {
+	std::weak_ptr< CPushDlg> weakSelf = this->shared_from_this();
+
+	TEduRecordParam para;
+	para.classId = 13582;
+	para.AppProc = "TICDemo.exe";
+	mLocalRecorder->startLocalRecord(para, "E:\\test\\test.flv", [this, weakSelf](TICModule module, int code, const char *desc) {
+		std::shared_ptr<CPushDlg> self = weakSelf.lock();
+		if (!self)
+			return;
+
+		if (code != 0) {
+			//AfxMessageBox(_T("开始录制失败"), MB_OK);
+		}
+		else {
+			//AfxMessageBox(_T("开始录制"), MB_OK);
+		}
+	});
+}
+
+void CPushDlg::stopRecord() {
+	std::weak_ptr< CPushDlg> weakSelf = this->shared_from_this();
+	mLocalRecorder->stopLocalRecord([this, weakSelf](TICModule module, int code, const char *desc) {
+		std::shared_ptr<CPushDlg> self = weakSelf.lock();
+		if (!self)
+			return;
+
+		if (code != 0) {
+			//AfxMessageBox(_T("停止录制失败"), MB_OK);
+		}
+		else {
+			//AfxMessageBox(_T("停止录制"), MB_OK);
+		}
+	});
+}
+
+
+void CPushDlg::pauseRecord() {
+	std::weak_ptr< CPushDlg> weakSelf = this->shared_from_this();
+	mLocalRecorder->pauseLocalRecord([this, weakSelf](TICModule module, int code, const char *desc) {
+		std::shared_ptr<CPushDlg> self = weakSelf.lock();
+		if (!self)
+			return;
+
+		if (code != 0) {
+			//AfxMessageBox(_T("暂停录制失败"), MB_OK);
+		}
+		else {
+			//AfxMessageBox(_T("暂停录制"), MB_OK);
+		}
+	});
+}
+
+void CPushDlg::resumeRecord() {
+	std::weak_ptr< CPushDlg> weakSelf = this->shared_from_this();
+	mLocalRecorder->resumeLocalRecord([this, weakSelf](TICModule module, int code, const char *desc) {
+		std::shared_ptr<CPushDlg> self = weakSelf.lock();
+		if (!self)
+			return;
+
+		if (code != 0) {
+			//AfxMessageBox(_T("暂停录制失败"), MB_OK);
+		}
+		else {
+			//AfxMessageBox(_T("暂停录制"), MB_OK);
+		}
+	});
+}
+
+void  CPushDlg::getRecord() {
+	const RecordKey key = RecordKey(13582, std::string(""), std::string(""));
+	std::weak_ptr< CPushDlg> weakSelf = this->shared_from_this();
+	mLocalRecorder->getRecordResult(key, [this, weakSelf](TICModule module, int code, const char *desc) {
+		std::shared_ptr<CPushDlg> self = weakSelf.lock();
+		if (!self)
+			return;
+
+		if (code == 0) {
+		}
+	});
+}
+
+void CPushDlg::OnLvnItemchangedLocalRecord(NMHDR *pNMHDR, LRESULT *pResult)
+{
+	LPNMLISTVIEW pNMLV = reinterpret_cast<LPNMLISTVIEW>(pNMHDR);
+
+	*pResult = 0;
+}
+
+
+void CPushDlg::OnNMDblclkLocalRecord(NMHDR *pNMHDR, LRESULT *pResult)
+{
+	LPNMITEMACTIVATE pNMItemActivate = reinterpret_cast<LPNMITEMACTIVATE>(pNMHDR);
+	
+	*pResult = 0;
+}
+
+
+void CPushDlg::OnLvnItemchangedListLocalRecord(NMHDR *pNMHDR, LRESULT *pResult)
+{
+	LPNMLISTVIEW pNMLV = reinterpret_cast<LPNMLISTVIEW>(pNMHDR);
+	// TODO: 在此添加控件通知处理程序代码
+	*pResult = 0;
+}
--- PaaS/Windows/Demo/PushDlg.h
+++ PaaS/Windows/Demo/PushDlg.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <Live/ITXLivePlayer.h>
 #include "..\SDK\TIC\localrecord\TICLocalRecord.h"
+#include "afxcmn.h"
 
 class CPushDlg
 	: public CDialogEx
@@ -22,12 +23,24 @@ private:
 	void StopPlay();
 	virtual BOOL OnInitDialog() override;
 	virtual void DoDataExchange(CDataExchange* pDX) override;
+	void UpdateFileList();
+
+	void initRecord();
+	void startRecord();
+	void stopRecord();
+	void pauseRecord();
+	void resumeRecord();
+	void getRecord();
 
 public:
 	ITXLivePlayer* mLivePlayer;
 	CButton chkPushEnable_;
+	CListCtrl mListRecord;
 	TICLocalRecorder* mLocalRecorder;
 public:
 	afx_msg void OnBnClickedChkEnableDraw();
 	afx_msg void OnBnClickedCheckEnablePush();
+	afx_msg void OnLvnItemchangedLocalRecord(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnNMDblclkLocalRecord(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnLvnItemchangedListLocalRecord(NMHDR *pNMHDR, LRESULT *pResult);
 };
